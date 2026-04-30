38c99c8ce10d1befbd10859c8d704ea778092363
Ismaaeel Ahmed <ismaaeelahmed11@gmail.com>
2026-04-30 22:14:17 +0100

    Complete File permissions -Challenge 3

---
+++ bash-scripting/file-checker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "==File Checker=="
+echo ""
+
+read -p "Enter File Name to Check: " filename
+
+if [ -e "$filename" ]; then
+echo "File '$filename' exists "
+else
+echo "File '$filename' does not exist "
+exit 1
+fi
+
+if [ -r "$filename" ]; then
+echo "File '$filename' is readable"
+else
+echo "File '$filename' is not readable"
+fi
+
+if [ -w "$filename" ]; then
+echo "File '$filename' is writable"
+else
+echo "File '$filename' is not writable"
+fi
+
+if [ -x "$filename" ]; then
+echo "File '$filename' is executable"
+else
+echo "File '$filename' is not executable"
+fi
